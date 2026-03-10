d0b6b1d44f7f38e7b6c78e3897fb7a8f7db54359
Charly CATIN--RICO <charly.catin-rico@etu.univ-nantes.fr>
2026-03-10 08:15:56 +0100

    init

---
+++ jeu.ts
@@ -0,0 +1,57 @@
+// toutes les pièces
+type Pion = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+type Fou = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+type Tour = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+type Cavalier = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+
+type Roi = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+type Reine = {
+    x : number;
+    y : number;
+    couleur : string;
+    isDead : boolean;
+}
+
+type Role = {
+    
+}
+
+
+type Piece = {
+    role : 
+    isDead : boolean;
+};
+
+plateau : Array<Array<number>>;
+
+
